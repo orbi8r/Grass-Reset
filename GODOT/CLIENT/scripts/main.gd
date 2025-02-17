983cf39d5d705e74270505c27151c1593b6c9c65
Shuban Biswas (orbi8r) <157096929+orbi8r@users.noreply.github.com>
2025-02-17 20:11:41 +0530

    Supabase plugin

---
+++ GODOT/CLIENT/scripts/main.gd
@@ -0,0 +1,24 @@
+extends Node
+
+# Nodes
+@onready var email: LineEdit = $AspectRatioContainer/Control/VBoxContainer/Email
+@onready var password: LineEdit = $AspectRatioContainer/Control/VBoxContainer/Password
+@onready var output: TextEdit = $AspectRatioContainer/Control/VBoxContainer/HBoxContainer/Output
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_register_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_login_pressed() -> void:
+	pass # Replace with function body.
