4016a940c7235596f71b19b84d8031f270684ac3
Shuban Biswas (orbi8r) <157096929+orbi8r@users.noreply.github.com>
2025-02-18 18:47:40 +0530

    LIVE Update again

--- GODOT/CLIENT/scripts/main.gd
+++ GODOT/CLIENT/scripts/main.gd
@@ -9,6 +9,7 @@ extends Node
 func _ready() -> void:
 	Supabase.auth.connect("signed_up", Callable(self, "_on_signed_up"))
 	Supabase.auth.connect("signed_in", Callable(self, "_on_signed_in"))
+	Supabase.auth.connect("error", Callable(self, "_on_error"))
 
 
 func _process(delta: float) -> void:
@@ -16,18 +17,42 @@ func _process(delta: float) -> void:
 
 
 func _on_register_pressed() -> void:
+	if !is_email_valid() or !is_password_valid():
+		return
 	Supabase.auth.sign_up(email.text, password.text)
+	output.text = "loading"
 
 
 func _on_login_pressed() -> void:
+	if !is_email_valid() or !is_password_valid():
+		return
 	Supabase.auth.sign_in(email.text, password.text)
+	output.text = "loading"
 
 
-func _on_signed_up():
-	print("Signed up!")
-	output.text = "Signed Up!"
+func is_password_valid() -> bool:
+	if password.text.length() < 6:
+		output.text = "password size must be Greater than 6"
+		return 0
+	output.text = ""
+	return 1
 
 
-func _on_signed_in():
-	print("Signed in!")
-	output.text = "Signed In!"
+func is_email_valid() -> bool:
+	if !email.text.ends_with("iiit.ac.in"):
+		output.text = "enter a valid iiit.ac.in email"
+		return 0
+	output.text = ""
+	return 1
+
+
+func _on_signed_up(user : SupabaseUser):
+	output.text = "Signed Up as "+str(user)
+
+
+func _on_signed_in(user : SupabaseUser):
+	output.text = "Signed In as "+str(user) 
+
+
+func _on_error(Error):
+	output.text = "Error : "+str(Error)
